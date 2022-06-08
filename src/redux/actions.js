7809439c4e94f94376ec16bee46897bd21a13e00
Huntsama <jawad.itoube@gmail.com>
2022-06-08 14:34:35 +0100

    setting up redux and function add journal and changing tilte and icon

---
+++ src/redux/actions.js
@@ -0,0 +1,8 @@
+export const Add_Item = "Add_Item"
+
+export const addItem = (item) => ({
+    type:Add_Item,
+    payload:item
+
+})
+
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,10 +1,29 @@
+import { Add_Item } from './actions';
+
 const intialState = {
+  journalItems: [],
+};
 
-}
+const journalReducer = (state = intialState, action) => {
+  switch (action.type) {
+    case Add_Item:
+      let id = 1;
+      if (state.journalItems.length > 0) {
+        id = state.journalItems[0].id + 1;
+      }
+      let item = {
+        id: id,
+        title: action.payload.title,
+        date: action.payload.date,
+        journal: action.payload.journal,
+      };
+      return {
+        ...state,
+        journalItems: [item, ...state.journalItems],
+      };
+    default:
+      return state;
+  }
+};
 
-
-const journalReducer = (state = intialState , action ) => {
-    
-}
-
-export default journalReducer
+export default journalReducer;
